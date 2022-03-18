d21197dc57a8990e03306882e43fd06dcd2cee7b
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-18 13:26:12 +0530

    add IContacts.cs

---
+++ IContacts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    interface IContacts
+    {
+        public void addContact(string firstName, string lastName, string email, string phoneNumber, string address, string zip, string city, String state);
+        public void Edit(string Name);
+        public void delete(string name);
+        public void displayContact();
+    }
+}
