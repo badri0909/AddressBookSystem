517f16ba129f7a2d7a020db3c70ecdacff27856a
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-18 13:27:37 +0530

    add .

---
+++ AddressBookSyst/AddressBookSystem/AddressBookMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    public class AddressBookMain
+    {
+        string firstName;
+        string lastName;
+        string address;
+        string city;
+        string state;
+        int pinCode;
+        long phone_number;
+        string email;
+        public void AddressBook()
+        {
+            firstName = "Badri";
+            lastName = "Chowdhary";
+            address = "6-33,gandhi chowk wyra";
+            city = "Khammam";
+            state = "Telangana";
+            pinCode = 507165;
+            phone_number = 9398599965;
+            email = "singuveerbhadrarao@gmail.com";
+
+            Console.WriteLine("FirstName: " + firstName + "\nLastName: " + lastName + "\nAdress: " + address + "\nCity: " + city + "\nState: " + state + "\nPinCode: " + pinCode + "\nPhoneNUmber: " + phone_number + "\nEmailId: " + email);
+        }
+        public void AddressBook(string name, string lastName, string address, string city, string state, int pinCode, long contact, string email)
+        {
+            firstName = name;
+            lastName = lastName;
+            address = address;
+            city = city;
+            state = state;
+            pinCode = pinCode;
+            phone_number = contact;
+            email = email;
+            Console.WriteLine("Hi " + firstName + " " + lastName + " your details have been saved");
+        }
+    }
+}
